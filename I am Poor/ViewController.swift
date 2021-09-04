efac23690693cfbf95684e104ac9b723c2a8017d
Matthew Piedra <matthewluispiedra@gmail.com>
2021-09-04 16:14:21 -0400

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Matthew Piedra on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
